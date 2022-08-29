dae6cdde57badfdf0f99563f26b0b65edc03c5f3
bbakercello <bbakercello@gmail.com>
2022-08-29 07:41:35 -0400

    server.js initialized at PORT

---
+++ server.js
@@ -0,0 +1,26 @@
+// importing express - method-override
+const express = require('express')
+const methodOverride = require('method-override')
+
+//CONTROLLER IMPORTS
+// const controllers = require('./controllers/post_controller')
+
+//app configuration
+const app = express()
+const PORT = 6000
+app.set('view engine','ejs')
+
+// Middleware
+app.use(express.static('public'))
+app.use(methodOverride('_method'));
+
+// app.use('/', controllers)
+
+//home route
+// app.get('/')
+
+
+//SERVER
+app.listen(6000, () => console.log('starting server at port:', PORT))
+
+// module.exports = app
