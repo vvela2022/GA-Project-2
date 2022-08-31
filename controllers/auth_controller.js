160d208444e40c9568a6427e395c7c0587a59b73
bbakercello <bbakercello@gmail.com>
2022-08-31 08:08:35 -0400

    login route initialized sucessfully

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -3,8 +3,14 @@ const router = express.Router();
 const bcrypt = require("bcryptjs");
 const { User } = require("../models");
 
+// register route
 router.get("/register", function (req, res) {
     return res.render("auth/register");
   });
+
+// login route
+router.get("/login", function (req, res) {
+  res.render("auth/login");
+});
 
 module.exports = router;
