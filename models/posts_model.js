2e4353cd2dc71dea0fe08e22202043733e65479f
“vvela2022” <victoriavela01@gmail.com>
2022-09-01 09:34:47 -0500

    begin user model creation

--- models/posts_model.js
+++ models/posts_model.js
@@ -13,6 +13,241 @@ const seedData = [
     {   user: 'Victoria Vela',
         body: 'That was such an awesome hike!',
         image: 'https://i.imgur.com/gJZu9kY.jpg',
-        location: 'Bryce Canyon, Utah'
-    }
+        location: 'Bryce Canyon, UT'
+    },
+    {
+        user: 'Ben Baker',
+        body: 'Wow, what a view!',
+        image: 'https://i.imgur.com/FcggF7V.jpg',
+        location: 'Denver, CO'
+    },
+    {   user: 'Lives4Nature',
+        body: 'This is awesome!',
+        image: 'https://i.imgur.com/lLIeAtq.jpg',
+        location: 'Grand Canyon, AZ'
+    },
+    {   
+        user: 'In2Water',
+        body: 'Living my best life!',
+        image: 'https://i.imgur.com/jqFU1t2.jpg',
+        location: 'Crete, Greece'
+    },
+    {   
+        user: 'Travels4Life',
+        body: 'Had an amazing time soaking in the sun!',
+        image: 'https://i.imgur.com/fnfbuB5.jpg',
+        location: 'Clearwater Beach, FL'
+    },
+    {   
+        user: 'NatureisMyLife',
+        body: 'It doesn\'t get better than that view',
+        image: 'https://i.imgur.com/j0s7HZV.jpg',
+        location: 'Glacier National Park, MT'
+    },
+    {   
+        user: 'DreamsofLandscapes',
+        body: 'Had an amazing time in hiking in these woods!',
+        image: 'https://i.imgur.com/DRnUQnh.jpg',
+        location: 'Redwood National Park, CA'
+    },
+    {   
+        user: 'Lives4theSea',
+        body: 'We had an amazing weekend at this gem!',
+        image: 'https://i.imgur.com/5cfJDPK.jpg',
+        location: 'Huntington Beach, CA'
+    },
+    {   
+        user: 'WannabeinNature',
+        body: 'What an amazing weekend!',
+        image: 'https://i.imgur.com/QfbgRnS.jpg',
+        location: 'Dublin, Ireland'
+    },
+    {   
+        user: 'Nature4Life',
+        body: 'Had an amazing time hiking today!',
+        image: 'https://i.imgur.com/oAbGR0K.jpg',
+        location: 'Boulder, CO'
+    },
+    {   
+        user: 'NaturalAdventurer',
+        body: 'The most beautiful sunset ever!',
+        image: 'https://i.imgur.com/MhXDawx.jpg',
+        location: 'Santorini, Greece'
+    },
+    {   
+        user: 'Adventure4Life',
+        body:'Such an amazing trip. Can\'t wait to go back',
+        image: 'https://i.imgur.com/qKQSgtb.jpg',
+        location: 'Mali, Maldives'
+    },
+    {   
+        user: 'IHeartTravel',
+        body: 'What a great place to go with the family!',
+        image: 'https://i.imgur.com/VNNbQ5b.jpg',
+        location: 'Tampa, FL'
+    },
+    {   
+        user: 'WannaTravelMore',
+        body: 'Wow! What an amazing trip we had this summer. Such a beautiful hike! Cannot wait to go back!',
+        image: 'https://i.imgur.com/jOlUaSs.jpg',
+        location: 'Denver, CO'
+    },
+    {   
+        user: 'Travels2Much',
+        body: 'I loved it. Such an amazing travel destination. So much to do!',
+        image: 'https://i.imgur.com/qAqm6fR.jpg',
+        location: 'Madrid, Spain'
+    },
+    {   
+        user: 'Can\'tStopTravelling',
+        body: 'Wow! What an amazing place!',
+        image: 'https://i.imgur.com/XcTMyeQ.jpg',
+        location: 'Big Island, Hawaii'
+    },
+    {   
+        user: '2MuchAdventure',
+        body: 'Half a day in the sun. No regrets.',
+        image: 'https://i.imgur.com/nEXzIvM.jpg',
+        location: 'Arches National Park, UT'
+    },
+]
+
+
+//User Data
+
+// email: {
+//     type: String,
+//     required: [true, "Please Provide An Email Address."],
+//     unique: true,
+// },
+// password: {
+//     type: String,
+//     required: [true, "Please Provide A Password"],
+// },
+// username: { type: String, required: true, unique: true },
+// avatar: {
+
+
+const userData = [
+    {
+    email: 'dRobles@gmail.com',
+    password: 'David',
+    username: 'David Robles',
+    avatar: '',
+    },
+    {
+    email: 'vVela@gmail.com',
+    password: 'Victoria',
+    username: 'Victoria Vela',
+    avatar: '',
+    },
+    {
+    email: 'bBaker@gmail.com',
+    password: 'Ben',
+    username: '',
+    avatar: '',
+    },
+    {
+    email: '',
+    password: '',
+    username: '',
+    avatar: '',
+    },
+    {
+    email: '',
+    password: '',
+    username: '',
+    avatar: '',
+    },
+    {
+    email: '',
+    password: '',
+    username: '',
+    avatar: '',
+    },
+    {
+    email: '',
+    password: '',
+    username: '',
+    avatar: '',
+    },
+    {
+    email: '',
+    password: '',
+    username: '',
+    avatar: '',
+    },
+    {
+    email: '',
+    password: '',
+    username: '',
+    avatar: '',
+    },
+    {
+    email: '',
+    password: '',
+    username: '',
+    avatar: '',
+    },
+{
+    email: '',
+    password: '',
+    username: '',
+    avatar: '',
+    },
+    {
+    email: '',
+    password: '',
+    username: '',
+    avatar: '',
+    },
+    {
+    email: '',
+    password: '',
+    username: '',
+    avatar: '',
+    },
+    {
+    email: '',
+    password: '',
+    username: '',
+    avatar: '',
+    },
+    {
+    email: '',
+    password: '',
+    username: '',
+    avatar: '',
+    },
+    {
+    email: '',
+    password: '',
+    username: '',
+    avatar: '',
+    },
+    {
+    email: '',
+    password: '',
+    username: '',
+    avatar: '',
+    },
+    {
+    email: '',
+    password: '',
+    username: '',
+    avatar: '',
+    },
+    {
+    email: '',
+    password: '',
+    username: '',
+    avatar: '',
+    },
+    {
+    email: '',
+    password: '',
+    username: '',
+    avatar: '',
+    },
+
 ]
